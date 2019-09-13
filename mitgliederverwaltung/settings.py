69c239756dca5dd5cc4ad1516859b78482e4e761
root <bipo342@gmail.com>
2019-09-13 08:29:30 +0200

    bugs_131019

--- mitgliederverwaltung/settings.py
+++ mitgliederverwaltung/settings.py
@@ -26,10 +26,12 @@ SECRET_KEY = 'p+=w=*%d@tvsj=)kui-l8v%dyah*mnk58stbu)k!x72vblo*qw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'members.ovg.at']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'members.ovg.at', '46.163.79.47',]
 HOSTNAME = 'blabla'
 INTERNAL_IPS = ['localhost', '127.0.0.1']
 CORS_ORIGIN_ALLOW_ALL = True
+USE_X_FORWARDED_HOST = True
+
 
 # Application definition
 
@@ -100,12 +102,21 @@ DATABASES = {
      'sqlite': {
          'ENGINE': 'django.db.backends.sqlite3',
          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-     }
+     },
+     'productive': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': 'members',
+         'USER': 'admin',
+         'PASSWORD': 'GUr6W6p3',
+         'HOST': 'localhost',
+         'PORT': '',
+    },
  }
  
-DATABASES['default'] = DATABASES['postgresql']
 DATABASES['default'] = DATABASES['sqlite']
 DATABASES['default'] = DATABASES['debug']
+DATABASES['default'] = DATABASES['productive']
+DATABASES['default'] = DATABASES['postgresql']
 
 
 
